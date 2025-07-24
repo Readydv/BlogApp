246db6122b41d1522ddd589b401992cb3bf66331
Danil <daniil-vn@mail.ru>
2025-07-24 19:31:22 +0500

    Подключил логирование с помощью NLog

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -4,95 +4,119 @@ using BlogApp.Models;
 using BlogApp.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+// Инициализируем логгер до сборки приложения
+var logger = LogManager.Setup()
+    .LoadConfigurationFromFile("nlog.config") // Подключение конфигурации
+    .GetCurrentClassLogger();
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IPostService, PostService>();
-builder.Services.AddScoped<ITagService, TagService>();
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<IPostTagService, PostTagService>();
-builder.Services.AddScoped<IUserProfileService, UserProfileService>();
-
-builder.Services.AddDbContext<BlogContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Identity configuration
-builder.Services.AddIdentity<ApplicationUser, Role>(options =>
+try
 {
-    options.Password.RequiredLength = 6;
-    options.Password.RequireDigit = true;
-    options.Password.RequireNonAlphanumeric = false;
-    options.User.RequireUniqueEmail = true;
-})
-.AddEntityFrameworkStores<BlogContext>()
-.AddDefaultTokenProviders();
+    logger.Info("Запуск приложения...");
+    var builder = WebApplication.CreateBuilder(args);
 
-// Настройка куки
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Auth/Login";
-    options.AccessDeniedPath = "/Error/403";
-    options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromDays(30);
-    options.SlidingExpiration = true;
-});
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+    builder.Services.AddScoped<IPostService, PostService>();
+    builder.Services.AddScoped<ITagService, TagService>();
+    builder.Services.AddScoped<ICommentService, CommentService>();
+    builder.Services.AddScoped<IPostTagService, PostTagService>();
+    builder.Services.AddScoped<IUserProfileService, UserProfileService>();
 
-var app = builder.Build();
+    builder.Services.AddDbContext<BlogContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Настройка обработки ошибок
-app.UseStatusCodePagesWithReExecute("/Error/{0}");  // Для кодов 400-599
-app.UseExceptionHandler("/Error/500");
+    // Identity configuration
+    builder.Services.AddIdentity<ApplicationUser, Role>(options =>
+    {
+        options.Password.RequiredLength = 6;
+        options.Password.RequireDigit = true;
+        options.Password.RequireNonAlphanumeric = false;
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<BlogContext>()
+    .AddDefaultTokenProviders();
 
-// Конвейер middleware
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
+    // Настройка куки
+    builder.Services.ConfigureApplicationCookie(options =>
+    {
+        options.LoginPath = "/Auth/Login";
+        options.AccessDeniedPath = "/Error/403";
+        options.Cookie.HttpOnly = true;
+        options.ExpireTimeSpan = TimeSpan.FromDays(30);
+        options.SlidingExpiration = true;
+    });
+
+    builder.Logging.ClearProviders(); //Убрал стандартные логгеры
+    builder.Logging.SetMinimumLevel(LogLevel.Information); //Логирую все от уровня инфо
+    builder.Host.UseNLog(); //Само подключение
+
+    var app = builder.Build();
+
+    // Настройка обработки ошибок
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");  // Для кодов 400-599
     app.UseExceptionHandler("/Error/500");
-    app.UseHsts();
+
+    // Конвейер middleware
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error/500");
+        app.UseHsts();
+    }
+
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    // Seed roles and users
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<BlogContext>();
+            context.Database.Migrate();
+
+            var roleManager = services.GetRequiredService<RoleManager<Role>>();
+            var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+            // Явно вызываем асинхронный метод сидирования
+            DataSeeder.SeedRolesAndUsersAsync(roleManager, userManager).GetAwaiter().GetResult();
+            logger.Info("Миграция базы данных и сидирование выполнены успешно.");
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"Ошибка при миграции или сидировании: {ex.Message} \n {ex.StackTrace}");
+        }
+    }
+
+    app.Run();
 }
-
-app.UseStatusCodePagesWithReExecute("/Error/{0}");
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-// Seed roles and users
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var services = scope.ServiceProvider;
-
-    try
-    {
-        var context = services.GetRequiredService<BlogContext>();
-        context.Database.Migrate();
-
-        var roleManager = services.GetRequiredService<RoleManager<Role>>();
-        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-
-        // Явно вызываем асинхронный метод сидирования
-        DataSeeder.SeedRolesAndUsersAsync(roleManager, userManager).GetAwaiter().GetResult();
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, $"Ошибка при миграции или сидировании: {ex.Message} \n {ex.StackTrace}");
-    }
+    // Лог фатальных ошибок
+    logger.Fatal(ex, "Прилоожениие завершилось аварийно при запуске");
+    throw;
 }
-
-app.Run();
-
+finally
+{
+    LogManager.Shutdown(); //Очищаю ресурсы логгера
+}
 
 
